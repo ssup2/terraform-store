79707b043f38e624f93e856466c9b490b4b3e644
ssup2 <supsup5642@gmail.com>
2023-07-22 22:37:07 +0900

    Fix examples

--- aws-eks-emr/examples/spark-submit.sh
+++ aws-eks-emr/examples/spark-submit.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-
+spark-submit \
+ --class org.apache.spark.examples.SparkPi \
+ --master k8s://87C2A505AF21618F97F402E454E530AF.yl4.ap-northeast-2.eks.amazonaws.com \
+ --conf spark.kubernetes.container.image=895885662937.dkr.ecr.us-west-2.amazonaws.com/spark/emr-6.10.0:latest \
+ --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark \
+ --deploy-mode cluster \
+ --conf spark.kubernetes.namespace=spark \
+ local:///usr/lib/spark/examples/jars/spark-examples.jar 20
