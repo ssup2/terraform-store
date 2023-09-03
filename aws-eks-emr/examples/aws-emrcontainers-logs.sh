dc8d24efb24084d2d90d7be4f516f8a0926ec4f8
ssup2 <supsup5642@gmail.com>
2023-09-04 01:08:58 +0900

    Update examples

--- aws-eks-emr/examples/aws-emrcontainers-logs.sh
+++ aws-eks-emr/examples/aws-emrcontainers-logs.sh
@@ -9,30 +9,10 @@ aws emr-containers start-job-run \
    --job-driver '{
      "sparkSubmitJobDriver":{
        "entryPoint": "local:///usr/lib/spark/examples/src/main/python/pi.py",
-       "sparkSubmitParameters": "--conf spark.executor.instances=2 --conf spark.executor.memory=2G --conf spark.executor.cores=1 --conf spark.driver.cores=1"
+       "sparkSubmitParameters": "--conf spark.driver.cores=1 --conf spark.driver.memory=512M --conf spark.executor.instances=1 --conf spark.executor.memory=512M --conf spark.executor.cores=1"
      }
    }' \
    --configuration-overrides '{
-     "applicationConfiguration": [
-       {
-         "classification": "spark-defaults",
-         "properties": {
-           "job-start-timeout":"1800",
-           "spark.ui.prometheus.enabled":"true",
-           "spark.executor.processTreeMetrics.enabled":"true",
-           "spark.kubernetes.driver.annotation.prometheus.io/scrape":"true",
-           "spark.kubernetes.driver.annotation.prometheus.io/path":"/metrics/executors/prometheus/",
-           "spark.kubernetes.driver.annotation.prometheus.io/port":"4040",
-           "spark.kubernetes.driver.service.annotation.prometheus.io/scrape":"true",
-           "spark.kubernetes.driver.service.annotation.prometheus.io/path":"/metrics/driver/prometheus/",
-           "spark.kubernetes.driver.service.annotation.prometheus.io/port":"4040",
-           "spark.metrics.conf.*.sink.prometheusServlet.class":"org.apache.spark.metrics.sink.PrometheusServlet",
-           "spark.metrics.conf.*.sink.prometheusServlet.path":"/metrics/driver/prometheus/",
-           "spark.metrics.conf.master.sink.prometheusServlet.path":"/metrics/master/prometheus/",
-           "spark.metrics.conf.applications.sink.prometheusServlet.path":"/metrics/applications/prometheus/"
-         }
-       }
-     ],
      "monitoringConfiguration": {
        "persistentAppUI": "ENABLED", 
        "cloudWatchMonitoringConfiguration": {
